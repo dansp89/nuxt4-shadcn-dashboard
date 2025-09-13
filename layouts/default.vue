f053b62b8b36442869f8d2f2c76556e8d2d0056e
DanSP <daniel.adg1337@gmail.com>
2025-09-13 04:36:36 -0300

    update: add preload in loader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import Preloader from '@/components/ui/loader/preloader.vue'
+
+const isClient = ref(false)
+
+onMounted(() => {
+  isClient.value = true
+})
 </script>
 
 <template>
-  <SidebarProvider>
-    <LayoutAppSidebar />
-    <SidebarInset>
-      <LayoutHeader />
-      <div class="min-w-0 w-full flex-1 overflow-x-auto p-4 lg:p-6">
-        <slot />
-      </div>
-    </SidebarInset>
-  </SidebarProvider>
+  <ClientOnly>
+    <SidebarProvider>
+      <LayoutAppSidebar />
+      <SidebarInset>
+        <LayoutHeader />
+        <div class="min-w-0 w-full flex-1 overflow-x-auto p-4 lg:p-6">
+          <slot />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+    <template #fallback>
+      <Preloader />
+    </template>
+  </ClientOnly>
 </template>
-
-<style scoped>
-
-</style>
